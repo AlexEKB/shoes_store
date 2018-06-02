f4b0c4ad38b772bbe688cfaf5e006ee8c191636b
alexekb <pu4kom@mail.ru>
2018-06-02 23:34:51 +0500

    redirect and table of orders ready

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,10 +1,18 @@
 class OrdersController < ApplicationController
+  #def index
+  #end
   skip_before_action :verify_authenticity_token, only: [:create]
+
+  http_basic_authenticate_with name: "alex", password: "09031989", only: :index
+
+  def index
+    @orders = Order.all
+  end
 
   def create
     @order = Order.new(params.require(:order).permit!)
     @order.save
 
-    redirect_to '/'
+    redirect_to '/ty'
   end
 end
